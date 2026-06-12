72b87569bbf84da51d331081609fb410269a13cc
Ronak Das <ronakdas2006@gmail.com>
2026-06-12 13:45:51 +0530

    fix port for render

--- server/index.js
+++ server/index.js
@@ -73,6 +73,7 @@ io.on('connection', (socket) => {
   })
 })
 
-server.listen(3001, () => {
-  console.log('Signaling server running on http://localhost:3001')
+const PORT = process.env.PORT || 3001
+server.listen(PORT, () => {
+  console.log(`Signaling server running on port ${PORT}`)
 })
